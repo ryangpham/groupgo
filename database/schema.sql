dd3641e500229b17a626e282c1c1af1f2dd1b001
AndriiFil2006 <filandrey06@gmail.com>
2026-03-21 23:31:17 -0400

    Add users, trips, roles, and memberships schemas

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,44 @@
 -- Initial schema goes here.
+
+
+CREATE TABLE users(
+	user_id SERIAL PRIMARY KEY,
+	
+	email VARCHAR(100) UNIQUE NOT NULL,
+	display_name VARCHAR(50) NOT NULL,
+	
+	is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE trips(
+	trip_id SERIAL PRIMARY KEY,
+	
+	trip_name VARCHAR(50) NOT NULL	, 
+	start_date DATE,
+	end_date DATE,
+	
+	owner_user_id INTEGER NOT NULL,
+	
+	FOREIGN KEY (owner_user_id) REFERENCES users(user_id),
+	
+	CHECK (end_date IS NULL OR start_date IS NULL OR end_date >= start_date)
+);
+
+CREATE TABLE roles(
+	role_id SERIAL PRIMARY KEY,
+	role_name VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE memberships(
+	user_id INTEGER,
+	trip_id INTEGER,
+	
+	role_id INTEGER NOT NULL,
+	joined_at DATE DEFAULT CURRENT_DATE,
+	
+	PRIMARY KEY(user_id, trip_id),
+	
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+	FOREIGN KEY (trip_id) REFERENCES trips(trip_id) ON DELETE CASCADE,
+	FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
